1244b79de3a0503e5842e8109b1b71688713cd67
kalidation <kalidation@gmail.com>
2021-10-08 17:41:39 +0200

    Splash Screen By Khaled

--- app/src/main/java/fr/mastergime/meghasli/escapegame/MainActivity.kt
+++ app/src/main/java/fr/mastergime/meghasli/escapegame/MainActivity.kt
@@ -2,10 +2,25 @@ package fr.mastergime.meghasli.escapegame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import fr.mastergime.meghasli.escapegame.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private val splashFragment = SplashFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.splash_fragment, splashFragment)
+            commit()
+        }
+
     }
 }
