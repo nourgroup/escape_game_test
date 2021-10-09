f42c18c593e714fa8c5a0dd966a66798869886a7
kalidation <kalidation@gmail.com>
2021-10-09 19:19:27 +0200

    Splash Screen finis, Login et Register interfaces finies.
    By khaled

---
+++ app/src/main/java/fr/mastergime/meghasli/escapegame/ui/fragments/SignUpFragment.kt
@@ -0,0 +1,12 @@
+package fr.mastergime.meghasli.escapegame.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import fr.mastergime.meghasli.escapegame.R
+
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
