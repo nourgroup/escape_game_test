7c66cbbd269662c03203a1a46e4458d151026269
mohamed meghasli <meghaslimohamed.said@hotmail.com>
2021-10-14 14:07:39 +0200

    version 0.022 with firebase connected

--- app/src/main/java/fr/mastergime/meghasli/escapegame/ui/fragments/SplashFragment.kt
+++ app/src/main/java/fr/mastergime/meghasli/escapegame/ui/fragments/SplashFragment.kt
@@ -1,9 +1,9 @@
 package fr.mastergime.meghasli.escapegame.ui.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import fr.mastergime.meghasli.escapegame.R
 import fr.mastergime.meghasli.escapegame.databinding.FragmentSplashBinding
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 
 class SplashFragment : Fragment() {
 
@@ -26,15 +26,18 @@ class SplashFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //
 
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            // Your Code
-            findNavController().navigate(R.id.action_splashFragment_to_logFragment)
-        }, 3000)
+        GlobalScope.launch(Dispatchers.IO) {
+            val currentThread = Thread.currentThread()
+            Log.d("mythread","$currentThread")
+            delay(3000)
+            withContext(Dispatchers.Main){
+                findNavController().navigate(R.id.action_splashFragment_to_logFragment)
+            }
+        }
 
 
 
